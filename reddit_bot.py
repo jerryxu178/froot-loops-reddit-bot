d70b92bf661cd6b77033503f254adc668787bf06
jerryxu178 <lx46@cornell.edu>
2017-11-03 21:44:27 -0400

    initial commit

---
+++ reddit_bot.py
@@ -0,0 +1,23 @@
+import praw
+import config
+import time
+
+def bot_login():
+	r = praw.Reddit(username = config.username,
+		password = config.password,
+		client_id = config.client_id,
+		client_secret = config.client_secret,
+		user_agent = "gost bee's froot loop bot v0.1")
+	return r
+
+def run_bot(r):
+	for comment in r.subreddit('test').comments(limit = 25):
+		if "fruit loop" in comment.body:
+			print "string found!"
+			comment.reply("You've misspelled froot loops!")
+	time.sleep(10)
+# [here](www.google.com)
+# comment.id
+# comment.author
+r = bot_login()
+run_bot(r)
