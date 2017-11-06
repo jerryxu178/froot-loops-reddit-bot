51677ed9b4ec7ae0c4c64134f3089167a0cf9321
jerryxu178 <lx46@cornell.edu>
2017-11-06 14:59:49 -0500

    updated

--- reddit_bot.py
+++ reddit_bot.py
@@ -7,19 +7,22 @@ def bot_login():
 		password = config.password,
 		client_id = config.client_id,
 		client_secret = config.client_secret,
-		user_agent = "gost bee's froot loop bot v0.1")
+		user_agent = "froot loop bot v1.0")
 	return r
 
 def run_bot(r):
 	for comment in r.subreddit('test').comments(limit = 25):
-		if "fruit loop" in comment.body:
-			print "string found!"
-			comment.reply("You've misspelled froot loops!")
-	time.sleep(10)
+		if "fruit loop" in comment.body and comment.author != config.username:
+			print "misspelling found!"
+			comment.reply("""You've misspelled froot loops! Believe it or not, 
+				[froot loops](https://en.wikipedia.org/wiki/Froot_Loops) 
+				contain no real fruit and are actually all the same flavor""")
+			time.sleep(10)
 
 # [here](www.google.com)
 # comment.id
 # comment.author
 
 r = bot_login()
+# while true:
 run_bot(r)
