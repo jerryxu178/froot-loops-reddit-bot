2e84dbdcaa40020723cae9a1f0b54b2788b32743
jerryxu178 <lx46@cornell.edu>
2017-11-06 14:59:32 -0500

    changed login cred

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
+"""username = "froot-loops-bot"
+password = "frootloops"
 username = "gost_bee"
 password = "carmel12452"
 client_id = "oWSNQ5R7alHr4A"
 client_secret = "0a1MTu_9DUQ5V4w_ARElpYoWp9M"
+"""
+
+
+username = "froot-loops-bot"
+password = "kelloggs"
+client_id = "omGarPuufHlfJw"
+client_secret = "pYnD_IPpQyYZD5U86qUX58oLD-Y"
