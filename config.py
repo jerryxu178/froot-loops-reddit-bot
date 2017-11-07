9ff7b61abd2a07add2e68082da2688e8d324df9b
jerryxu178 <lx46@cornell.edu>
2017-11-07 15:18:49 -0500

    added documentation

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
-"""username = "froot-loops-bot"
-password = "frootloops"
-username = "gost_bee"
-password = "carmel12452"
-client_id = "oWSNQ5R7alHr4A"
-client_secret = "0a1MTu_9DUQ5V4w_ARElpYoWp9M"
-"""
 
+#This file contains the login information and credentials needed to run the bot.
 
 username = "froot-loops-bot"
 password = "kelloggs"
 client_id = "omGarPuufHlfJw"
 client_secret = "pYnD_IPpQyYZD5U86qUX58oLD-Y"
+
+sub_reddit = "test"
--- reddit_bot.py
+++ reddit_bot.py
@@ -3,6 +3,7 @@ import config
 import time
 
 def bot_login():
+# log into the reddit account with credentials from config.py
 	r = praw.Reddit(username = config.username,
 		password = config.password,
 		client_id = config.client_id,
@@ -11,18 +12,15 @@ def bot_login():
 	return r
 
 def run_bot(r):
-	for comment in r.subreddit('test').comments(limit = 25):
-		if "fruit loop" in comment.body and comment.author != config.username:
+# run the bot on subreddit designated in config.py
+	for comment in r.subreddit(config.sub_reddit).comments(limit = 25):
+		if "fruit loops" in comment.body and comment.author != config.username:
 			print "misspelling found!"
 			comment.reply("""You've misspelled froot loops! Believe it or not, 
 				[froot loops](https://en.wikipedia.org/wiki/Froot_Loops) 
-				contain no real fruit and are actually all the same flavor""")
+				contain no real fruit and are actually all the same flavor.""")
 			time.sleep(10)
 
-# [here](www.google.com)
-# comment.id
-# comment.author
-
 r = bot_login()
-# while true:
-run_bot(r)
+while True:
+	run_bot(r)
