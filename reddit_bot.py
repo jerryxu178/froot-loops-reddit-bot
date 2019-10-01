c7aa4e5bebfbf962d7fc5119b062e8fe217c3507
jerryxu178 <lx46@cornell.edu>
2019-10-01 15:04:34 -0400

    whitespace change

--- reddit_bot.py
+++ reddit_bot.py
@@ -4,23 +4,23 @@ import time
 
 def bot_login():
 # log into the reddit account with credentials from config.py
-	r = praw.Reddit(username = config.username,
-		password = config.password,
-		client_id = config.client_id,
-		client_secret = config.client_secret,
-		user_agent = "froot loop bot v1.0")
-	return r
+    r = praw.Reddit(username = config.username,
+        password = config.password,
+        client_id = config.client_id,
+        client_secret = config.client_secret,
+        user_agent = "froot loop bot v1.0")
+    return r
 
 def run_bot(r):
 # run the bot on subreddit designated in config.py
-	for comment in r.subreddit(config.sub_reddit).comments(limit = 25):
-		if "fruit loops" in comment.body and comment.author != config.username:
-			print "misspelling found!"
-			comment.reply("""You've misspelled froot loops! Believe it or not, 
-				[froot loops](https://en.wikipedia.org/wiki/Froot_Loops) 
-				contain no real fruit and are actually all the same flavor.""")
-			time.sleep(10)
+    for comment in r.subreddit(config.sub_reddit).comments(limit = 25):
+        if "fruit loops" in comment.body and comment.author != config.username:
+            print "misspelling found!"
+            comment.reply("""You've misspelled froot loops! Believe it or not, 
+                [froot loops](https://en.wikipedia.org/wiki/Froot_Loops) 
+                contain no real fruit and are actually all the same flavor.""")
+            time.sleep(10)
 
 r = bot_login()
 while True:
-	run_bot(r)
+    run_bot(r)
